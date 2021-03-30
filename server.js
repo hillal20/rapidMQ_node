281f7a4b162f3509e9588b6193412e5d8f1f9abe
Ashraf Talukder <ashraf.talukder996@hotmail.com>
2021-03-29 23:43:01 -0500

    latest

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const mq = require("amqplib")
+
+const server = express()
+server.use(bodyParser.json())
+
+
+
+mq.connect("amqp://localhost")
+.then(conn => {
+    conn.createChannel()
+    .then(ch => {
+        const queue = "test_1"
+        ch.assertQueue(queue)
+        .then( q => {
+            console.log("q ==> ", q)
+        })
+        .catch(err => {
+            console.log("queue err ==> ", err)
+        })
+        //sending message 
+        ch.sendToQueue(queue, Buffer.from(JSON.stringify({name:'hilal', age:'30'}), 'utf8'))
+        ch.consume(queue,  msg => {
+            
+            const obj = msg.content.toString();
+            console.log("msg ==> ", obj)
+        
+    }, {
+        noAck:true
+    })
+       
+       
+
+
+
+    })
+    .catch(err => {
+        console.log(" channel err => ", err)
+    })
+
+})
+.catch(err => {
+console.log(" connection err ===> ", err)
+})
+
+
+server.get("/message", (req,res)=> {
+
+    res.status(200).json({data:"***  api is working ***"})
+})
+
+
+server.listen(4000, ()=> {
+    console.log(" === server is on port 4000 ===")
+})
